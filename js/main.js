20fa24d47cec4e51042888eabb1de055a66f7914
subo-9439 <kws3363@gmail.com>
2021-05-02 23:19:57 +0900

    swiper

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("hi");
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click',function(){
+searchEl.addEventListener('click',function(){ 
   searchInputEl.focus();
 });
 
@@ -24,7 +24,7 @@ const badgeEL = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function(){
   console.log(window.scrollY);
   if(window.scrollY > 500){
-    //gsap.to(요소,지속시간, 옵션);
+    //gsap.to(요소,지속시간, 옵션:보통객체데이터);
     gsap.to(badgeEL, .6 ,{
       opacity: 0,
       display: 'none'
@@ -41,3 +41,19 @@ window.addEventListener('scroll', _.throttle(function(){
     //badgeEL.style.display = 'block';
   }
 },300));
+
+//이미지 애니메이션
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fdaeEl,index){
+  gsap.to(fdaeEl, 1, {
+    delay: (index+1) * .7,  //0.7 1.4 2.1 2.7
+    opacity:1
+  });
+});
+
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container',{
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
